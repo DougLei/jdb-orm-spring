c3a18bd2427575486ba7cb4fff48247a3f5385d2
Administrator <Administrator@USER-20200530ZD>
2020-10-01 11:15:28 +0800

    t

--- src/main/java/com/douglei/orm/spring/redis/mapping/store/SpringRedisMappingContainer.java
+++ src/main/java/com/douglei/orm/spring/redis/mapping/store/SpringRedisMappingContainer.java
@@ -1,7 +1,6 @@
 package com.douglei.orm.spring.redis.mapping.store;
 
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.RedisSerializer;
 
 import com.douglei.orm.configuration.environment.mapping.container.MappingContainer;
 import com.douglei.orm.configuration.environment.mapping.container.impl.redis.RedisHandler;
@@ -14,13 +13,8 @@ import com.douglei.orm.configuration.environment.mapping.container.impl.redis.Re
 public abstract class SpringRedisMappingContainer extends RedisHandler implements MappingContainer {
 
 	protected RedisTemplate<String, Object> template;
-	protected RedisSerializer<String> keySerializer;
-	protected RedisSerializer<Object> valueSerializer;
 	
-	@SuppressWarnings("unchecked")
 	public void setTemplate(RedisTemplate<String, Object> template) {
 		this.template = template;
-		this.keySerializer = (RedisSerializer<String>) template.getKeySerializer();
-		this.valueSerializer = (RedisSerializer<Object>) template.getValueSerializer();
 	}
 }
